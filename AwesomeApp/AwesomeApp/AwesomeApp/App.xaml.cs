dc7a33febc01cf41e7af25ce83e6695ebac479e0
池田 里佳 <ikedarika@ad.se-project.co.jp>
2021-10-12 17:12:52 +0900

    First

---
+++ AwesomeApp/AwesomeApp/AwesomeApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AwesomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios={AwesomeApp.iOS};android={AwesomeApp.iOS};uwp={AwesomeApp.iOS};windowsdesktop={AwesomeApp.iOS}", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
